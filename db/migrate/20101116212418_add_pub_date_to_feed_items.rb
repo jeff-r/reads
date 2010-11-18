e47a421c6ec496e05a9df5c85173a6fc56769eaa
Jeff Roush <jeff@jeffroush.com>
2010-11-18 10:15:40 -0800

    Changed datatype in a migration from DateTime to timestamp. Postgresql calls it timestamp; I'm moving from sqlite3 to pg so I can upload the app to Heroku.

--- db/migrate/20101116212418_add_pub_date_to_feed_items.rb
+++ db/migrate/20101116212418_add_pub_date_to_feed_items.rb
@@ -1,6 +1,6 @@
 class AddPubDateToFeedItems < ActiveRecord::Migration
   def self.up
-    add_column :feed_items, :pub_date, :DateTime
+    add_column :feed_items, :pub_date, :timestamp
   end
 
   def self.down
