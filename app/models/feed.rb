0ead9ca1525681c72f35ff8302659f3ec917f99b
Jeff Roush <jeff@jeffroush.com>
2010-12-23 11:50:43 -0800

    Tinkered with the colors and layout of feed title area.

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -6,7 +6,7 @@ class Feed < ActiveRecord::Base
     if feed_items.count == 0
       return DateTime.parse "01-01-1970"
     end
-    lasttime = feed_items.order("pub_date").last.pub_date
+    lasttime = feed_items.order("pub_date").last.pub_date.strftime("%m/%d/%Y at %I:%M %p")
   end
 
   def top_level_items
